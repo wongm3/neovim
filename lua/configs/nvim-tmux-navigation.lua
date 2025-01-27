ec83fcd8847343980bbf90ec9070555b7da69261
Matt Wong <matt.wong.rpi@gmail.com>
2025-01-27 16:15:07 -0500

    move config for tmux nav

---
+++ lua/configs/nvim-tmux-navigation.lua
@@ -0,0 +1,10 @@
+return {
+  keybindings = {
+    left = "<C-h>",
+    down = "<C-j>",
+    up = "<C-k>",
+    right = "<C-l>",
+    last_active = "<C-\\>",
+    next = "<C-Space>",
+  }
+}
--- lua/plugins/nvim-tmux-navigation.lua
+++ lua/plugins/nvim-tmux-navigation.lua
@@ -1,17 +1,4 @@
 return {
   'alexghergh/nvim-tmux-navigation',
-  config = function()
-    local nvim_tmux_nav = require 'nvim-tmux-navigation'
-
-    nvim_tmux_nav.setup {
-      keybindings = {
-        left = "<C-h>",
-        down = "<C-j>",
-        up =  "<C-k>",
-        right = "<C-l>",
-        last_active = "<C-\\>",
-        next = "<C-Space>"
-      }
-    }
-  end
+  opts = require 'configs.nvim-tmux-navigation'
 }
