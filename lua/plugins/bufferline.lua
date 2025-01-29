59beb68f0828cb313906e21c214fe8a29bd328a8
Matt Wong <matt.wong.rpi@gmail.com>
2025-01-28 22:57:12 -0500

    add diagnostics to bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,7 +4,12 @@ return {
     dependencies = 'nvim-tree/nvim-web-devicons',
     opts = {
       options = {
+        diagnostics = 'nvim_lsp',
         always_show_bufferline = true,
+        diagnostics_indicator = function(count, level)
+          local icon = level:match("error") and " " or " "
+          return " " .. icon .. count
+        end,
         offsets = {
           {
             filetype = 'neo-tree',
